2ea91092892ba67d11594c14ca98ab376818233f
alodor <jose-a-mendez@hotmail.com>
2019-06-17 17:11:01 -0400

    Inicialización del repositorio

---
+++ vue.js
@@ -0,0 +1,19 @@
+new Vue({
+    el: '#app',
+    data: {
+        langs: [
+            { name: 'Python'},
+            { name: 'C#'},
+            { name: 'PHP'},
+            { name: 'Ruby'},
+            { name: 'JavaScript'},
+            { name: 'Java'},
+            { name: 'Dart'},
+            { name: 'Kotlin'},
+            { name: 'C'},
+            { name: 'Go'},
+            { name: 'Swift'},
+            { name: 'Bash'}
+        ]
+    }
+})
